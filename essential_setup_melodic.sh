efc880da7ecb017c2c614e86298edb7684a320fd
Wookjin Ahn <lexqrt2@pusan.ac.kr>
2022-04-22 22:19:19 +0900

    Update essential_setup_melodic.sh

--- essential_setup_melodic.sh
+++ essential_setup_melodic.sh
@@ -6,6 +6,16 @@ sudo apt-get upgrade -y
 echo "install VGA driver"
 apt-get install nvidia-driver-470 -y
 
+echo "install essential"
+sudo apt-get install build-essential git wget curl pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev -y
+
+echo "install cmake"
+sudo apt-get install libssl-dev -y
+wget https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0.tar.gz
+tar -xvf cmake-3.21.0.tar.gz
+cd cmake-3.21.0
+./bootstrap && make && sudo make install
+
 echo "install CLion IDE"
 sudo snap install clion --classic 
 
@@ -19,9 +29,6 @@ echo "install Google Chrome"
 wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm -rf ./google-chrome-stable_current_amd64.deb
-
-echo "install essential"
-sudo apt-get install build-essential cmake git wget curl pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev -y
 
 echo "install python3"
 sudo apt-get install python3 python3-pip -y
