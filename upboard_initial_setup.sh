f4f1569fd8efe742fc455133659ede9b0af4b441
Wookjin Ahn <lexqrt2@pusan.ac.kr>
2023-10-05 14:59:52 +0900

    remove qt dependency

--- upboard_initial_setup.sh
+++ upboard_initial_setup.sh
@@ -22,7 +22,7 @@ echo "."
 echo "."
 echo "."
 echo -e "$COLOR_GREEN --------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |   INSTALL 00/12   | $COLOR_END"
+echo -e "$COLOR_GREEN |   INSTALL 00/11   | $COLOR_END"
 echo -e "$COLOR_GREEN |  start installing | $COLOR_END"
 echo -e "$COLOR_GREEN --------------------- $COLOR_END"
 
@@ -30,13 +30,13 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 01/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 01/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |      Essential      | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo apt-get install build-essential git wget gpg curl pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev -y
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 02/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 02/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |        Cmake        | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 cd $INSTALL_DIR
@@ -48,19 +48,19 @@ cd cmake-3.21.0
 cd .. && sudo rm -rf cmake-3.21.0 && sudo rm -rf cmake-3.21.0.tar.gz
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 03/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 03/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |        CLion        | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo snap install clion --classic
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 04/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 04/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |        Slack        | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo snap install slack --classic
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 05/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 05/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |        Chrome       | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -68,31 +68,31 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm -rf ./google-chrome-stable_current_amd64.deb
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 06/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 06/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |       Python3       | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo apt-get install python3 python3-pip -y
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 07/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 07/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |      terminator     | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo apt-get install terminator -y
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 08/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 08/11    | $COLOR_END"
 echo -e "$COLOR_GREEN | simplescreenrecorder| $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo apt-get install simplescreenrecorder -y
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 09/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 09/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |        Eigen        | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 sudo apt-get install libeigen3-dev
 
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 10/12    | $COLOR_END"
+echo -e "$COLOR_GREEN |    INSTALL 10/11    | $COLOR_END"
 echo -e "$COLOR_GREEN |         RBDL        | $COLOR_END"
 echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
 cd $INSTALL_DIR
@@ -106,16 +106,10 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DRBDL_BUILD_ADDON_URDFREADER=ON
 make
 sudo make install
 
-echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |    INSTALL 11/12    | $COLOR_END"
-echo -e "$COLOR_GREEN |         Qt5         | $COLOR_END"
-echo -e "$COLOR_GREEN ----------------------- $COLOR_END"
-sudo apt-get install qtcreator -y
-sudo apt-get install qt5-default -y
 echo
 echo
 echo -e "$COLOR_GREEN --------------------- $COLOR_END"
-echo -e "$COLOR_GREEN |   INSTALL 12/12   | $COLOR_END"
+echo -e "$COLOR_GREEN |   INSTALL 11/11   | $COLOR_END"
 echo -e "$COLOR_GREEN |     installed     | $COLOR_END"
 echo -e "$COLOR_GREEN --------------------- $COLOR_END"
 
